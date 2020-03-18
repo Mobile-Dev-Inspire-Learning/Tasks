c43c42730b49354018582ca1ab3f748550669a50
alaarg <ahmadalaarg123@gmail.com>
2020-03-18 10:54:37 +0200

    upload recycle view task

---
+++ KotlinRecyclerView/app/src/main/java/com/alaarg/kotlinrecyclerview/CustomAdapter.kt
@@ -0,0 +1,38 @@
+package com.alaarg.kotlinrecyclerview
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class CustomAdapter(val userList: ArrayList<User>) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        val user: User = userList[p1]
+
+        p0?.textViewName?.text = user.name
+        p0?.textViewAddress?.text = user.address
+    }
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val v = LayoutInflater.from(p0?.context).inflate(R.layout.list_layout, p0, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textViewName: TextView = itemView.findViewById(R.id.textViewName)
+        val textViewAddress: TextView = itemView.findViewById(R.id.textViewaddress)
+
+
+    }
+
+
+}
---
+++ KotlinRecyclerView/app/src/main/java/com/alaarg/kotlinrecyclerview/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.alaarg.kotlinrecyclerview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView : RecyclerView = findViewById(R.id.recicileView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this , LinearLayout.VERTICAL, false)
+
+        val users = ArrayList<User>()
+
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+        users.add((User("inspire" , " jordan")))
+
+
+
+
+
+val adapter = CustomAdapter(users)
+        recyclerView.adapter = adapter
+    }
+}
